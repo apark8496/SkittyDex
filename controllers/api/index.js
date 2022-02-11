4f7da078bb04f6b6aed4d8943e31c8f36b27ad25
Ashley <adunderwood96@gmail.com>
2022-02-10 19:22:48 -0500

    seeds

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
-const teamRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
 
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
-router.use('/team', teamRoutes)
+router.use('/post', postRoutes)
 
 module.exports = router;
