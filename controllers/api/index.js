7160ea5225c9524daa1a04d181cc1559a0f1b2c5
Anija <mflores0572@gmail.com>
2022-02-10 20:07:45 -0500

    made some minor changes to allow server to run

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
-const teamRoutes = require('./post-routes.js');
+const userRoutes = require("./user-routes.js");
+const postRoutes = require("./post-routes.js");
+const commentsRoutes = require("./post-routes.js");
 
-
-router.use('/users', userRoutes);
-router.use('/comments', commentRoutes);
-router.use('/team', teamRoutes)
+router.use("/users", userRoutes);
+router.use("/post", postRoutes);
+router.use("/comments", commentsRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,39 +1,39 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
 User.hasMany(Post, {
-    foreignKey: 'userID'
+  foreignKey: "userID",
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'userID'
+  foreignKey: "userID",
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'userID',
-    onDelete: 'cascade',
-    hooks:true
+  foreignKey: "userID",
+  onDelete: "cascade",
+  hooks: true,
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'postID',
-    onDelete: 'cascade',
-    hooks: true
+  foreignKey: "postID",
+  onDelete: "cascade",
+  hooks: true,
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'userID',
-    onDelete: 'cascade',
-    hooks:true
+  foreignKey: "userID",
+  onDelete: "cascade",
+  hooks: true,
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'postID',
-    onDelete: 'cascade',
-    hooks:true
-})
+  foreignKey: "postID",
+  onDelete: "cascade",
+  hooks: true,
+});
 
 // Insert TEAM INFO
 
-module.exports = { User, Post, Comment, Team };
+module.exports = { User, Post, Comment };
