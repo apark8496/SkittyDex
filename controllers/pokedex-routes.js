738f63277af53181762cac682db8d00c1c7ed5c7
Ashley <adunderwood96@gmail.com>
2022-02-13 16:35:12 -0500

    style and basic pokedex route added

--- controllers/pokedex-routes.js
+++ controllers/pokedex-routes.js
@@ -3,4 +3,30 @@ const sequelize = require('../config/connection');
 const { Pokedex } = require('../models');
 const withAuth = require('../utils/auth');
 
+// get all Pokedex
+router.get('/', withAuth, (req, res) => {
+    Pokedex.findAll({
+      where: {
+        userID: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'name',
+        'types',
+        'superEffective',
+        'notEffective',
+      ],
+         })
+      .then(PokedexData => {
+        const Pokedexs = PokedexData.map(Pokedex => Pokedex.get({ plain: true }));
+        res.render('Pokedex', { Pokedexs, logged_in: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
+module.exports = router;
+
+// TODO:I have no idea if this is the correct method for it show pokemon on the new pokedex page or not
